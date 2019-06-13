0cb3eb7ac612b231490ce1a91794e5831768e351
marcinzaraj <marcinzaraj@gmail.com>
2019-06-13 20:56:44 +0200

    Initial commit

---
+++ src/main/java/com/example/pkpai/PkpaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pkpai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkpaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PkpaiApplication.class, args);
+    }
+
+}
